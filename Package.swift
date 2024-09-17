37291b59f87cbf5ea58ce526e26fa73d5a8ca074
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-09-17 09:19:13 -0400

    Turning on concurrency warnings

--- Package.swift
+++ Package.swift
@@ -1,23 +1,24 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "LanguageServerScripts",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "LanguageServerScripts",
-            targets: ["LanguageServerScripts"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "LanguageServerScripts"),
-        .testTarget(
-            name: "LanguageServerScriptsTests",
-            dependencies: ["LanguageServerScripts"]),
-    ]
+	name: "LanguageServerScripts",
+	products: [
+		.library(name: "LanguageServerScripts", targets: ["LanguageServerScripts"]),
+	],
+	targets: [
+		.target(name: "LanguageServerScripts"),
+		.testTarget(name: "LanguageServerScriptsTests", dependencies: ["LanguageServerScripts"]),
+	]
 )
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency")
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
